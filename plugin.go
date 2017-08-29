e27b6876ee63cb7acde0b0cb2c84f1ce4f205960
Christian Kniep <christian@qnib.org>
2017-08-29 14:25:26 +0200

    moved to github.com/qframe/...

--- plugin.go
+++ plugin.go
@@ -4,23 +4,26 @@ import (
 	"fmt"
 	"github.com/zpatrick/go-config"
 
-	"github.com/qnib/qframe-types"
-	"github.com/qnib/qframe-utils"
+	"github.com/qframe/types/qchannel"
+	"github.com/qframe/types/plugin"
+	"github.com/qframe/types/constants"
+	"github.com/qframe/types/messages"
+	"reflect"
 )
 
 const (
 	version   = "0.1.5"
-	pluginTyp = qtypes.HANDLER
+	pluginTyp = qtypes_constants.HANDLER
 	pluginPkg = "log"
 )
 
 type Plugin struct {
-	qtypes.Plugin
+	*qtypes_plugin.Plugin
 }
 
-func New(qChan qtypes.QChan, cfg *config.Config, name string) (Plugin, error) {
+func New(qChan qtypes_qchannel.QChan, cfg *config.Config, name string) (Plugin, error) {
 	p := Plugin{
-		Plugin: qtypes.NewNamedPlugin(qChan, cfg, pluginTyp, pluginPkg, name, version),
+		Plugin: qtypes_plugin.NewNamedPlugin(qChan, cfg, pluginTyp, pluginPkg, name, version),
 	}
 	p.Version = version
 	p.Name = name
@@ -31,17 +34,16 @@ func New(qChan qtypes.QChan, cfg *config.Config, name string) (Plugin, error) {
 func (p *Plugin) Run() {
 	p.Log("notice", fmt.Sprintf("Start log handler v%s", p.Version))
 	bg := p.QChan.Data.Join()
-	inputs := p.GetInputs()
 	for {
 		select {
 		case val := <-bg.Read:
 			switch val.(type) {
-			case qtypes.QMsg:
-				qm := val.(qtypes.QMsg)
-				if len(inputs) != 0 && !qutils.IsInput(inputs, qm.Source) {
-					continue
-				}
-				p.Log("info" , fmt.Sprintf("%-7s sType:%-6s sName:[%d]%-10s %s\n", qm.LogString(), qm.Type, qm.SourceID, qm.Source, qm.Msg))
+			case qtypes_messages.Message:
+				qm := val.(qtypes_messages.Message)
+				qm.StopProcessing(p.Plugin, false)
+				p.Log("info" , fmt.Sprintln(qm.ToJSON()))
+			default:
+				p.Log("info", fmt.Sprintf("Dunno type '%s': %v", reflect.TypeOf(val), val))
 			}
 		}
 	}
